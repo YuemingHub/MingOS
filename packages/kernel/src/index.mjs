c60325f520fae6111da484f7e9ceb5a820903f74
YuemingHub <176864547@qq.com>
2026-08-06 22:51:08 +0800

    feat: export source review invariant

--- packages/kernel/src/index.mjs
+++ packages/kernel/src/index.mjs
@@ -1,2 +1,2 @@
-export { checkActorReferences, checkAuthorization, checkContextLedger, checkContinuityBundle, checkHandoffReferences, checkSpaceReferences, checkTaskAuthorization, checkTaskCompletion } from './invariants.mjs';
+export { checkActorReferences, checkAuthorization, checkContextLedger, checkContinuityBundle, checkHandoffReferences, checkSourceReview, checkSpaceReferences, checkTaskAuthorization, checkTaskCompletion } from './invariants.mjs';
 export { validateSchema } from './schema-validator.mjs';
